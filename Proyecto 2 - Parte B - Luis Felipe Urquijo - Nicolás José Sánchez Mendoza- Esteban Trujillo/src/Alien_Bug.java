2ce2213aa3693c4b78c0d9902b4d536152d36047
EsteTruji <82886890+EsteTruji@users.noreply.github.com>
2021-05-28 22:57:06 -0500

    Delete Proyecto 2 - Parte B - Luis Felipe Urquijo - Nicolás José Sánchez Mendoza- Esteban Trujillo directory

--- Proyecto 2 - Parte B - Luis Felipe Urquijo - Nicolás José Sánchez Mendoza- Esteban Trujillo/src/Alien_Bug.java
+++
@@ -1,6 +0,0 @@
-public class Alien_Bug extends Bug {
-
-    public Alien_Bug() {
-        super("AB", 20);
-    }
-}
--- Proyecto 2 - Parte B - Luis Felipe Urquijo - Nicolás José Sánchez Mendoza- Esteban Trujillo/src/Normal_Bug.java
+++
@@ -1,7 +0,0 @@
-public class Normal_Bug extends Bug {
-
-    public Normal_Bug() {
-        super("NB", 10);
-    }
-
-}
